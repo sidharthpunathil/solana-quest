7cce9e4679ac327b6ef141121d22d4b6144084b0
Sidharth Punathil <31244727+sidharthpunathil@users.noreply.github.com>
2022-01-14 22:58:46 +0530

    added quest0

---
+++ quest0/hello-world/src/main.rs
@@ -0,0 +1,82 @@
+fn main() {
+    
+    // let mut x: i32 = 5;
+    // println!("Hello, world! {}", x);
+    // x = 5;
+    // println!("Hello, world 2 {}", x);
+
+    // let a = (1, 2, 3);
+    // let(val_1, val_2, _) = a;
+    // println!("{} {}",val_1, val_2)
+
+    // Array 
+    // let a = [1, 2, 3];
+    // println!("{} ",a[1])
+
+    // let a: [i32; 10] = [1, 2, 3];
+    // println!("{:?} ",a)
+
+    // Conditions
+
+    // let num = 1;
+
+    // if num == 1 {
+    //     println!("Yes");
+    // } else {
+    //     "NO";
+    // }
+
+    //loops
+
+    // let mut x = 1;
+
+    // loop {
+    //     x = x * 2;
+    //     if x >= 500 {
+    //         break;
+    //     } else {
+    //         println!("The value of x is {}", x);
+    //     }
+    // }
+
+    // while loop 
+    // let mut x = 1;
+
+    // while x <= 500 {
+    //     x = x * 2;
+    //     println!("The value of x is {}", x);
+    // }
+
+    // for x in 0..10 { // right side is exclusive
+    //     println!("The value of x is {}", x);
+    // }
+    
+    // for x in 0..=10 { // right side is exclusive
+    //     println!("The value of x is {}", x);
+    // }
+
+    // Iterator 
+
+    // let x = [1, 2, 3];
+    // for each in x {
+    //     println!{"{}", each}
+    // }
+
+    // let x = 1;
+
+    // match x {
+    //     1 => println("The value of x is 1"),
+    //     2 => println("The value of x is 2"),
+    //     _ => println("The value of x is invalid"),
+    // }
+        
+    // let x = false;
+    // let y = true;
+
+    // match (x, y) {
+    //     (true, true) => println!("wow all true"),
+    //     (false, false) => println!("wow all true"),
+    //     (true, false) => println!("wow true false"),
+    //     _ => println!("invalid combination")
+    // }
+}
