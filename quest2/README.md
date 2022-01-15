55c9b9dd2f8e1cc74b243a647992120c2bac051b
Sidharth Punathil <31244727+sidharthpunathil@users.noreply.github.com>
2022-01-15 15:26:57 +0530

    added quest2

---
+++ quest2/README.md
@@ -0,0 +1,1 @@
+Create a roulette game in Solana
---
+++ quest2/index.js
@@ -0,0 +1,43 @@
+const web3 = require("@solana/web3.js");
+
+// Generating a wallet instance for secret key
+// const userWallet = web3.Keypair.fromSecretKey(Uint8Array.from(userSecretKey));
+// console.log(userWallet);
+
+// console.log(`Signature is ${signature}`);
+
+
+const transferSOL = async (from, to, transferAmt) => {
+    try {
+        const connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed");
+
+        const transaction = new web3.Transaction().add(
+            web3.SystemProgram.transfer({
+                fromPubkey: new web3.PublicKey(from.publicKey.toString()),
+                toPubkey: new web3.PublicKey(to.publicKey.toString()),
+                lamports: transferAmt*web3.LAMPORTS_PER_SOL
+            })
+        )
+
+        const signature = await web3.sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [from]
+        );
+
+        return signature
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const getWalletBalance = async (pubk) => {
+    try {
+        const connection = new web3.Connection(web3.clusterApiUrl("devnet"), "confirmed");
+        const balance = await connection.getBalance(new web3.PublicKey(pubk));
+        return balance/web3.LAMPORTS_PER_SOL;
+    } catch (err) {
+        console.log(err);
+    }
+};
